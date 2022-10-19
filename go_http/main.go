123c1d4b658a3350ff4afcf54b6865e4f71ffb96
umair-abdulla <107067065+umair-abdulla@users.noreply.github.com>
2022-10-19 17:14:17 +0530

    Add files via upload

---
+++ go_http/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("wrote", len(bs), "bytes")
+	return len(bs), nil
+}
